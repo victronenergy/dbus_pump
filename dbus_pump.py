f3eb6d58c692ac06355a7b7d12705873284b79b1
Jesús Miguel Pérez Fernandez <jperez@victronenergy.com>
2016-07-19 17:26:51 +0200

    Version bumped v0.2

--- dbus_pump.py
+++ dbus_pump.py
@@ -30,7 +30,7 @@ from dbusmonitor import DbusMonitor
 from settingsdevice import SettingsDevice
 from logger import setup_logging
 
-softwareversion = '0.1'
+softwareversion = '0.2'
 
 
 class DbusPump:
