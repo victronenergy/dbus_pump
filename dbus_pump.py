b9b8deacc1bf4a818904ac5d176d7c4d7bdc9d4b
Izak Burger <isburger@gmail.com>
2021-10-20 10:18:31 +0200

    v0.8

--- dbus_pump.py
+++ dbus_pump.py
@@ -30,7 +30,7 @@ from dbusmonitor import DbusMonitor
 from settingsdevice import SettingsDevice
 from logger import setup_logging
 
-softwareversion = '0.7'
+softwareversion = '0.8'
 
 
 class DbusPump(object):
